0ce09e52c30cfc31cf3671d7a08de00145bbbc6d
Viktoria Kirichenko <viktoria.kirichenko@student.kuleuven.be>
2023-03-20 14:48:56 +0100

    cleaned

--- Data/Arcene.R
+++
@@ -1,4 +0,0 @@
-# ARCENE 
-#description: http://archive.ics.uci.edu/ml/machine-learning-databases/arcene/Dataset.pdf
-
-data<-read.csv("C:/Users/viki1/Documents/Uni/Thesis/Thesis/Data/arcene_test.data")
